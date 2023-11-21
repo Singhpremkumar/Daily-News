5c10e5d25e32c56622a5112c9091cb78c74c1659
Singhpremkumar <ps7739775@gmail.com>
2023-11-21 12:03:51 +0530

    formatting the code and adding new functions in it

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -11,7 +11,7 @@ export class NewsItem extends Component {
             <div className="card-body">
               <h5 className="card-title">{title}<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{source}</span></h5>
               <p className="card-text">{description}</p>
-              <p className="card-text"><small className="text-muted">By {!author? "Unknown" :author} on {new Date (date).toGMTString()}</small> </p>
+              <p className="card-text"><small className="text-primary">By {!author? "Unknown" :author} on {new Date (date).toGMTString()}</small> </p>
               <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read more</a>
             </div>
         </div>
