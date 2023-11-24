9103c5f6d9a34b6d9a159a6e1bfe56f5b03ebc14
Singhpremkumar <ps7739775@gmail.com>
2023-11-24 12:07:53 +0530

    formatting the code

--- src/component/News.js
+++ src/component/News.js
@@ -33,11 +33,15 @@ export class News extends Component {
   }
 
   async updateNews() {
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0334f7ab66e54a1c9beba99bf11d4ed4&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
+    this.props.setProgress(30);
     let data = await fetch(url);
     let parsedData = await data.json();
+    this.props.setProgress(60);
     this.setState({ articles: parsedData.articles, totalArticles: parsedData.totalResults, loading: false })
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
@@ -45,7 +49,7 @@ export class News extends Component {
   }
 
   fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 })
+    // this.setState({ page: this.state.page + 1 })
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0334f7ab66e54a1c9beba99bf11d4ed4&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     // this.setState({ loading: true });
     let data = await fetch(url);
